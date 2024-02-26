c796e11b08e21ebbb47f018ad459d4de338dabbe
Parsa Gachkar /pʌrsʌ gæʧ'kʌr/ <parsagachkar@gmail.com>
2024-02-26 04:37:21 +0330

    Add project files.

---
+++ BlazorPrerenderingSolution/HydratingComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp4
+{
+    public class HydratingComponent:ComponentBase
+    {
+        [Inject] private IHttpContextAccessor HttpContextAccessor { get; set; } = default!;
+
+        protected sealed override void OnInitialized()
+        {
+            base.OnInitialized();
+            if (!HttpContextAccessor.HttpContext!.Response.HasStarted) PreRendering();
+            else Hydrating();
+        }
+
+        protected virtual void Hydrating()
+        {
+        }
+
+        protected virtual void PreRendering()
+        {
+        }
+
+        protected sealed override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            if (!HttpContextAccessor.HttpContext!.Response.HasStarted) await PreRenderingAsync();
+            else await HydratingAsync();
+        }
+
+        protected virtual Task HydratingAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected virtual Task PreRenderingAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
